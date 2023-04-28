9bad14ddf5930711c604525a3b1cc7838bbfbe14
Axel Martinez <axel20ayalam@gmail.com>
2023-04-28 12:17:53 -0400

    subiendo repositorio

---
+++ ComunidadSegura/ComunidadSegura/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ComunidadSegura.Services;
+using ComunidadSegura.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ComunidadSegura
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
